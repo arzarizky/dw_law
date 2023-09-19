99f0846e24515d9ed1f0f6e262cdae19defd2579
Arza Rizky Nova Ramadhani <panggilsajarizky@gmail.com>
2023-09-19 08:46:10 +0700

    update biodata

--- pages/team/components/Bioadata.tsx
+++ pages/team/components/Bioadata.tsx
@@ -26,16 +26,9 @@ function Bioadata() {
   );
   return (
     <div className="md:p-20 p-12 bg-gray-100">
-      <h1 className="text-dvBlue text-4xl mb-2">Founder</h1>
+      <h1 className="text-dvBlue text-4xl mb-2">Founder and Managing Partner</h1>
       <hr className="border border-maroon w-founderUnderline mb-4" />
       <div className="md:flex gap-x-40">
-        <div className="md:mb-0 mb-5">
-          <TeamCard
-
-            name={"Destivano Wibowo, S.H., LL.M"}
-            position="Founder and Managing Partner"
-          />
-        </div>
         <div className="md:w-3/4">
           <Fade in={inViewport}>
             <Box ref={ref}>
@@ -107,10 +100,12 @@ function Bioadata() {
             </ListItem>
           </UnorderedList>
           <text className="font-semibold text-2xl">Contact</text>
-          <div className="flex gap-x-2 mt-4 items-center">
+          <h1 className="text-dvBlue mt-4 text-2xl">Destivano Wibowo, S.H., LL.M</h1>
+          <text className="text-sm">Founder and Managing Partner</text> 
+          <div className="flex gap-x-2  items-center">
             <Mail />
             <text className="text-lg text-dvBlue">
-              Ivan : ragil@dwalaw.co.id
+              Email : ivan@dwalaw.co.id
             </text>
           </div>
         </div>
